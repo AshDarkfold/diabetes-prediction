5677330e104e676814843e3a9b653919024f16e6
AshDarkfold <ashutosh.kaushik2018@vitstudent.ac.in>
2021-08-31 17:10:47 +0530

    init: django project and static files

---
+++ deployment/diabetes/views.py
@@ -0,0 +1,32 @@
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+@csrf_exempt
+def diabetes_pre(request):
+    template = loader.get_template('index.html')
+    pregnancies = request.POST.get("Pregnancies")
+    glucose = request.POST.get("Glucose")
+    bloodpressure = request.POST.get("BloodPressure")
+    skinthickness = request.POST.get("SkinThickness")
+    insulin = request.POST.get("Insulin")
+    BMI = request.POST.get("BMI")
+    DiabetesPedigreeFunction = request.POST.get("DiabetesPedigreeFunction")
+    age = request.POST.get("Age")
+
+    diabetes_data = [[pregnancies, glucose, bloodpressure, skinthickness, insulin, BMI, DiabetesPedigreeFunction, age]]
+    print(diabetes_data)
+    outcome = 0
+
+    if outcome == 1:
+        result = "Diabetic"
+    elif outcome == 0:
+        result = "Not Diabetic"
+
+    return HttpResponse(template.render({'result': result}))
